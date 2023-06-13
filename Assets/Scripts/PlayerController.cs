05b4e136418041974ecd72b745079543a0c2deb7
Batuhan <batuhan.tosun.96@hotmail.com>
2023-06-13 10:51:12 +0300

    Runner Control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runningSpeed;
+    float touchXDelta = 0;
+    float newX = 0;
+    public float xSpeed;
+    public float limitX;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwipeCheck();
+    }
+
+    private void SwipeCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+
+        transform.position = newPosition;
+    }
+}
