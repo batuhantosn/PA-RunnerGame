2da93e6d0a01fcc05600757781b0057f31d20d4e
Batuhan <batuhan.tosun.96@hotmail.com>
2023-06-13 23:52:56 +0300

    Obstacles and Animation Added

--- Assets/Scripts/CollisionCollect.cs
+++ Assets/Scripts/CollisionCollect.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollisionCollect : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI CoinText;
+
+    public PlayerController playerController;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
@@ -14,12 +17,18 @@ public class CollisionCollect : MonoBehaviour
             Destroy(other.gameObject);
             AddCoin();
         }
+        else if(other.CompareTag("End"))
+        {
+            Debug.Log("Win");
+            playerController.runningSpeed = 0;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Collision with obstacle");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
